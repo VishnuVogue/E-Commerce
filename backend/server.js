552b79b6d6445fc6e162997758b4c6bb556d35e2
VishnuVogue <vishnuvogue1@gmail.com>
2025-02-27 21:35:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ connectCloudinary()
 
 //middleware
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: *, // Allow only your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 //api endpoints
 app.use('/api/user',userRouter)
